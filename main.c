cf0c2c0d834ee7e26285db66185cb128ebe9f671
ToastyyX <erick.cool321@icloud.com>
2024-10-14 15:09:13 -0500

    initial commit

---
+++ main.c
@@ -0,0 +1,33 @@
+#include "RRL.h"
+
+int main() {
+        
+    // Number of processes
+    int PRCS = 0;
+
+    printf("%s", "Enter the number of processes: ");
+    scanf("%d", &PRCS);
+
+    // Predefined 'Burst Time', 'Arrival Time', and 'Time Slice'
+    int BT[PRCS];
+    int AT[PRCS];
+    int TS = 0;
+
+    // Burst Time
+    printf("%s", "Enter the burst time for each process: ");
+    for (int i = 0; i < PRCS; i++) { 
+        scanf("%d", &BT[i]);
+    }
+
+    // Arrival Time
+    printf("%s", "Enter the arrival time for each process: ");
+    for (int i = 0; i < PRCS; i++) { 
+        scanf("%d", &AT[i]);
+    }
+
+    // Time Slice
+    printf("%s", "Enter the time slice: ");
+    scanf("%d", &TS);
+
+    return 0;
+}
